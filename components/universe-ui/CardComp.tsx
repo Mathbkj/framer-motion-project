aa5665324f751038765b49cc274954b648b26da7
Mathbkj <matheusgblasel@hotmail.com>
2025-02-06 02:44:53 -0300

    New Features

--- components/universe-ui/CardComp.tsx
+++ components/universe-ui/CardComp.tsx
@@ -1,15 +1,15 @@
-import  { FC } from "react";
+import { FC } from "react";
 import { CardType } from "../../types/CardType";
-import {motion} from "motion/react";
+import { motion } from "motion/react";
 import { Button } from "../Button";
-export const CardComp: FC<CardType> = ({ title, content, btnText }) => {
-  return <motion.div className="text-white p-4 shadow-md inset-1 inset-shadow-white rounded-md inconsolata flex flex-col gap-2 text-center">
+
+export const CardComp: FC<CardType & {rotation?:number}> = ({ title, content, btnText,rotation }) => {
+  return <motion.div animate={{rotate:rotation}} whileHover={{scale:1.2,rotate:0}} className="text-black p-4 backdrop-blur-2xl bg-white rounded-md inconsolata flex flex-col gap-2 text-center">
     <motion.div className="flex flex-col gap-4">
       <h1 className="text-2xl font-bold">{title}</h1>
       <p className="text-base font-medium">{content}</p>
     </motion.div>
-    <br/>
-    <Button text={btnText} colors={{bgColor:"white",textColor:"black"}} type="button" />
-    
+    <br />
+    <Button colors={{bgColor:"white",textColor:"black"}} type="button" text={btnText} />
 </motion.div>
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { motion, useScroll } from "motion/react";
+import { motion, scroll, useMotionValue, useScroll, useSpring, useTransform } from "motion/react";
 import { ListType } from "../types/ListType";
 import { v4 } from "uuid";
 import { Typewriter } from "../components/Typewriter";
@@ -7,6 +7,7 @@ import { ArrowedButton } from "../components/ArrowedButton";
 import { MotionButton } from "../components/motion/MotionButton";
 import { CardComp } from "../components/universe-ui/CardComp";
 import einsteinImg from "./assets/einstein.png";
+import { useRef } from "react";
 
 function App() {
   const navList: Array<ListType> = [
@@ -48,13 +49,19 @@ function App() {
       ease: "easeOut",
     },
   };
-  const { scrollYProgress,scrollY } = useScroll();
-  const opacityVariant = {
-    opacity:scrollYProgress
-  }
+  const { scrollYProgress } = useScroll({
+    offset: ["0 1","2 3"],
+    layoutEffect:true,
+  });
+  const scale = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: .001
+  });
+  const opacity = useTransform(scale,[0.5,0.8,1],[0.7,0.9,1])
   return (
-    <>
-      <motion.nav className="w-full flex justify-around items-center">
+    <motion.div>
+      <motion.nav className="w-full sticky flex justify-around items-center">
         <motion.b
           initial={hidden}
           animate={visible}
@@ -63,20 +70,15 @@ function App() {
           Logo
         </motion.b>
         <motion.ul
+          
           initial={hiddenReverse}
           animate={visibleReverse}
           className="flex text-base gap-5 justify-around items-center"
         >
           {navList.map((item) =>
             item.type == "link" && item.content.title !== "Blog" ? (
-              <li className="relative my-2">
-                {/*<motion.button
-                  className="cursor-pointer relative z-10 inconsolata text-white/50 bg-slate-950 py-2 px-4 rounded-full m-2"
-                  type="button"
-                >
-                  {item.content.title}
-                </motion.button>*/}
-                <MotionButton text={item.content.title} />
+              <li className="relative my-2"> 
+               <MotionButton text={item.content.title} />
               </li>
             ) : item.type == "link" && item.content.title === "Blog" ? (
               <li className="relative my-2">
@@ -93,7 +95,7 @@ function App() {
         </motion.ul>
       </motion.nav>
       <main className="flex flex-col mt-10 mx-4 p-2">
-        <header className="flex flex-col justify-center items-center gap-2">
+        <motion.header className="flex flex-col justify-center items-center gap-2">
           <Typewriter
             text="Lorem ipsum dolor sit amet consectetur adipisicing elit"
             size={"4xl"}
@@ -111,28 +113,32 @@ function App() {
             vero quae quas iusto ab et velit. Ducimus ratione nesciunt officia
             vel et dolor iusto dicta qui culpa.
           </motion.p>
-          <ArrowedButton text="Learn More" />
-        </header>
-        <motion.section style={opacityVariant} className="md:grid md:w-[500px] flex flex-col mt-10 md:grid-cols-2 self-center block md:grid-rows-2 gap-6">
+          <ArrowedButton text="Start Playing" />
+        </motion.header>
+        <motion.section style={{scale:scale,opacity:opacity}} className="md:grid md:w-[500px] block mt-10 md:grid-cols-2 self-center md:grid-rows-2 gap-6">
           <CardComp
+            rotation={-45}
             title="Lorem Ipsum"
             content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
             provident repellendus, molestias commodi corrupti praesentium"
             btnText="Explore"
           />
-           <CardComp
+          <CardComp
+            rotation={45}
             title="Lorem Ipsum"
             content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
             provident repellendus, molestias commodi corrupti praesentium"
             btnText="Explore"
           />
-           <CardComp
+          <CardComp
+            rotation={-135}
             title="Lorem Ipsum"
             content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
             provident repellendus, molestias commodi corrupti praesentium"
             btnText="Explore"
           />
-           <CardComp
+          <CardComp
+            rotation={135}
             title="Lorem Ipsum"
             content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
             provident repellendus, molestias commodi corrupti praesentium"
@@ -140,18 +146,20 @@ function App() {
           />
         </motion.section>
         <br/>
-        <motion.div style={opacityVariant} className="flex mt-4 gap-2 justify-center items-center ">
-          <h1 className="text-transparent leading-13 flex-1 bg-gradient-to-br from-neutral-100 font-bold text-4xl to-neutral-800/20 bg-clip-text">Lorem ipsum dolor amet consectetur <br/>adipsicing ?</h1>
-          <p className="text-white/50 text-base text-wrap flex-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi et repellat, placeat tenetur totam quas quibusdam, non neque vero, cumque quos voluptas quo delectus! Ipsa velit sequi repudiandae tenetur hic omnis, totam dicta, ad sunt temporibus quaerat nobis expedita, ea dolorum voluptas labore distinctio deleniti explicabo! Laboriosam aspernatur ea natus facilis asperiores dolorum adipisci ipsam mollitia magni vitae assumenda blanditiis, porro perferendis et velit. Ut quae, dignissimos eius, laudantium culpa facilis sit neque inventore eveniet libero voluptate harums </p>
+        <motion.div style={{scale:scale}} className="flex md:flex-nowrap flex-wrap mt-8 justify-center items-center">
+          <h1 className="text-transparent bg-gradient-to-br flex-1 leading-12 from-neutral-100 font-bold text-4xl ms-3 to-neutral-800/20 bg-clip-text">Lorem ipsum dolor amet consectetur adipsicing ?</h1>
+          <p className="text-white/50 text-base text-wrap flex-1 shrink">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi et repellat, placeat tenetur totam quas quibusdam, non neque vero, cumque quos voluptas quo delectus! Ipsa velit sequi repudiandae tenetur hic omnis, totam dicta, ad sunt temporibus quaerat nobis expedita, ea dolorum voluptas labore distinctio deleniti explicabo! Laboriosam aspernatur ea natus facilis asperiores dolorum adipisci ipsam mollitia magni vitae assumenda blanditiis, porro perferendis et velit. Ut quae, dignissimos eius, laudantium culpa facilis sit neque inventore eveniet libero voluptate harums </p>
         </motion.div>
         <br />
-        <motion.div style={opacityVariant} className="flex justify-center items-center gap-8 mt-8 w-full text-black flex-row ">
-          <img width={400} height={400} src={einsteinImg} className=" ring rounded-full ring-offset-4 object-scale-down"></img>
-          <p className=" text-transparent bg-gradient-to-bl from-neutral-100 to-neutral-800/20 bg-clip-text inconsolata font-bold text-3xl text-wrap flex-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci labore ipsam assumenda non repellat architecto laborum nesciunt vitae rem necessitatibus molestias qui error minima exercitationem, deleniti optio facere doloremque ex.</p>
-        </motion.div>
       </main>
-      <footer></footer>
-    </>
+      
+      <footer className="w-full bg-white mt-5">
+      <motion.div style={{scale:scale}} className="flex p-10 bg-fixed flex-row-reverse md:flex-nowrap flex-wrap bg-origin-content justify-center items-center mt-8 text-black ">
+          <img width={300} height={300} src={einsteinImg} className="shrink ring-black rounded-full ring-offset-1 object-cover"></img>
+          <p className="inconsolata mx-10 font-bold text-2xl text-wrap">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci labore ipsam assumenda non repellat architecto laborum nesciunt vitae rem necessitatibus molestias qui error minima exercitationem, deleniti optio facere doloremque ex.</p>
+        </motion.div>
+      </footer>
+    </motion.div>
   );
 }
 
